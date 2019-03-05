0083fa55a25103515e45dc24542100a8f1d3a122
Alif Sajan <4084667+sendsent@users.noreply.github.com>
2019-03-05 14:24:25 -0800

    Updating/adding files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
 import Details from './components/Details';
 import Cart from './components/Cart';
-import Default from './components/Default';
+import Default from './components/Default'; 
 
 class App extends Component {
   render() {
